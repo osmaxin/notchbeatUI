63a1d972c22e0bf511e896edeee0cf07911b4343
godwinchibyk <godwin.chibyk@gmail.com>
2020-03-03 23:39:17 +0100

    header, nav and page banner section done

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,7 +3,12 @@ import '../sass/style.scss';
 const searchBox = document.querySelector(".search-box .popUp-container");
 
 const searchIcon = document.querySelector('.search-box .fa-search');
+const exitNavMenuBtn = document.querySelector('.nav-bar-container .go-back') 
+const navBarMenuContent = document.querySelector('.nav-bar-wrapper');
+const navBarMenuBtn = document.querySelector('nav .menu-icon');
 
+
+// This function shows and hides the search box on page header
 const toggleSearchBox = () => {
     const scl = searchBox.classList;
     if(scl.contains('hide-search-box')){
@@ -14,5 +19,16 @@ const toggleSearchBox = () => {
         scl.remove('show-search-box');
     }
 }
-
 searchIcon.addEventListener('click', toggleSearchBox);
+
+
+// This function shows and hides the navBarMenuContent
+const toggleNavBarMenuContent = () => {
+    if(searchBox.classList.contains('show-search-box')) {
+        searchBox.classList.remove('show-search-box');
+        searchBox.classList.add('hide-search-box');
+    }
+    navBarMenuContent.classList.toggle('show-nav-menu');
+};
+navBarMenuBtn.addEventListener('click', toggleNavBarMenuContent);
+exitNavMenuBtn.addEventListener('click', toggleNavBarMenuContent);
